0ed19c518df279947fbae7335d9ec84939fbe838
damenor <damenor.develop@gmail.com>
2020-09-18 04:30:45 +0200

    Init page example

---
+++ example/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+
+      <div>
+        <h1 className="app-title">@damenor/sass</h1>
+        <p className="app-subtitle">Utils for SASS and CSS</p>
+      </div>
+
+    </>
+  );
+}
+
+export default App;
